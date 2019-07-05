c390fd6fc3dd5b87e6b745e2e6137277c47d4bb1
Jakub Kąkolewski <jkakolewski@sii.pl>
2019-07-05 16:49:59 +0200

    removed unnecessary mapping from httpClient

--- src/main/java/pl/kuba565/RepositoryInfo/client/HttpClient.java
+++ src/main/java/pl/kuba565/RepositoryInfo/client/HttpClient.java
@@ -18,10 +18,6 @@ public class HttpClient {
     public RepositoryInfoDTO get(String URL) {
         RestTemplate restTemplate = new RestTemplateBuilder().build();
 
-        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM));
-        restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
-
         return restTemplate
                 .exchange(URL,
                         GET,
